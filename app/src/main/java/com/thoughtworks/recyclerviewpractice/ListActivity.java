b81f36afc2abef8f7bc95e9fe88a185bc3a474e1
qinghua <qinghua.liu@thoughtworks.com>
2020-07-22 21:11:19 +0800

    refactory data structure

--- app/src/main/java/com/thoughtworks/recyclerviewpractice/ListActivity.java
+++ app/src/main/java/com/thoughtworks/recyclerviewpractice/ListActivity.java
@@ -16,7 +16,7 @@ public class ListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
         RecyclerView recyclerView = findViewById(R.id.recycler_view_list);
-        List<Data> data = Data.creatMockData();
+        List<Data> data = Data.createMockData();
         MyAdapter myAdapter = new MyAdapter(data);
         recyclerView.setAdapter(myAdapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -26,20 +26,25 @@ public class ListActivity extends AppCompatActivity {
 }
 
 class Data {
+    public static final int TYPE_ITEM = 0;
+    public static final int TYPE_TITLE = 1;
     public String title;
     public String description;
     public int number;
+    public int type;
 
-    public Data(String title, String description, int number) {
+    public Data(String title, String description, int number, int type) {
         this.title = title;
         this.description = description;
         this.number = number;
+        this.type = type;
     }
 
-    public static List<Data> creatMockData() {
+    public static List<Data> createMockData() {
         ArrayList<Data> data = new ArrayList<>();
+        data.add(new Data("This is header", null, 0, TYPE_TITLE));
         for (int i = 0; i < 15; i++) {
-            data.add(new Data("title" + i, "desc" + i, i));
+            data.add(new Data("title" + i, "desc" + i, i, TYPE_ITEM));
         }
         return data;
     }
