91cfdc2cd610b381c33c5e84625009366bcef6c9
qinghua <qinghua.liu@thoughtworks.com>
2020-07-23 20:15:16 +0800

    practice use worker thread

--- app/src/main/java/com/thoughtworks/recyclerviewpractice/MainActivity.java
+++ app/src/main/java/com/thoughtworks/recyclerviewpractice/MainActivity.java
@@ -1,12 +1,16 @@
 package com.thoughtworks.recyclerviewpractice;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +22,37 @@ public class MainActivity extends AppCompatActivity {
                 createListActivity();
             }
         });
+        button = findViewById(R.id.thread_practice);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ButtonClickTask buttonClickTask = new ButtonClickTask();
+                buttonClickTask.execute();
+            }
+        });
+
     }
 
     public void createListActivity() {
         Intent intent = new Intent(this, ListActivity.class);
         startActivity(intent);
+    }
+
+    class ButtonClickTask extends AsyncTask<Void, Integer, Void> {
+        @Override
+        protected Void doInBackground(Void... voids) {
+            button.setClickable(false);
+            for (Integer i = 0; i < 10; i++) {
+                publishProgress(i);
+                SystemClock.sleep(1000);
+            }
+            button.setClickable(true);
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            button.setText(values[0].toString());
+        }
     }
 }
